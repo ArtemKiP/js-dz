c4ebf8245696d7817b1bcadee39b84a110ca1d9e
ArtemKiP <der.pap@mail.ru>
2023-02-21 17:29:28 +0600

    1-hm

---
+++ main.js
@@ -0,0 +1,124 @@
+// let a = [1,2,3,4,5];
+//
+// for (let i =0 ; i <= a.length; i++) {
+//     console.log(i)
+// }
+
+// let a = [1,2,3,4,5];
+//
+// let result = 0;
+//
+// for (let i = 0; i < a.length; i++){
+//     result += a[i] ;
+//
+// }
+// console.log(result);
+
+// let i = 0;
+// while (i < 3){
+//     console.log(`number ${i}!` );
+//     i++
+// }
+
+// let arr = [-2,7,9,5,8,1,12,15,17,3];
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 0 && arr[i] < 10){
+//         console.log(arr[i])
+//     }
+// }
+
+
+// for (let i = 1; i < 10; i++){
+//     i+= 1;
+//     console.log(i)
+// }
+
+// let result = 1;
+// for (let i = 0; i <= 100; i++){
+//     result += i
+// }
+//
+//
+// console.log(result);
+
+// let arr = ['BMW', 'Mersedes', 'Opel', 'Mazda', 'FIAT', 'Ferarri', 'Reno'];
+//
+// let newArr = [];
+//
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i]  ) {
+//         newArr = [...newArr , arr[i]]
+//
+//     }
+// }
+// console.log(newArr);
+
+// let arr = [2,3,4,5,6,7];
+//
+// let newArr = [];
+//
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] === 5){
+//         newArr = [...newArr, arr[i]];
+//         continue;
+//     }
+//     newArr = [...newArr, arr[i] * 2];
+//
+// }
+// for (let a = 0 ; a < newArr.length; a++){
+//     if (newArr[a] % 2 === 0){
+//         console.log(newArr[a]);
+//     }
+// }
+
+
+// let arr = [-2, 7, '9б', 'Vlad', 1, 12, 15, 'Ivan', 17, 3];
+//
+// for (let i = 0; i < arr.length; i++){
+//     if (typeof arr[i] === 'string'){
+//         console.log(arr[i])
+//     }
+// }
+
+
+// let arr = ['понедельник','вторник','среда','четверг','пятница','суббота','воскресение'];
+//
+// for (let i = 0;i < arr.length;i++){
+//     if (arr[i] === 'суббота' || arr[i] === 'воскресение'){
+//         console.log(arr[i] = 'выходной');
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+// let arr = ['Наташа','Анастасия','Дмитрий','Максим'];
+// arr.shift();
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+// let arr = ['Наташа', 'Анастасия', 2, 'Максим', 4];
+//
+// for (let i = 0; i < arr.length; i++){
+//     if (isFinite(arr[i])){
+//         console.log(arr[i])
+//     }
+//
+//
+// }
+// for (let i = 0; i < arr.length; i++){
+//     if (typeof arr[i] === 'string'){
+//         console.log(arr[i])
+//     }
+//
+//
+// }
+//
+// let a = 5;
+//
+//
+//
+// for (let i = 2 ; i <= 10; i++){
+//     console.log(a * i)
+// }
